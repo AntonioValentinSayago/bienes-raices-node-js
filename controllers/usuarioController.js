e7f3f44b39fb998b5d862e93f1133efae544a168
AntonioValentin <cesar_anto30@hotmail.com>
2023-08-21 18:28:36 -0600

    Validación del Formulario con Express-Validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from '../models/Usuario.js';
 
 const formualrioLogin = (req, res) => {
@@ -16,8 +17,24 @@ const formularioResgistro = (req, res) => {
 
 const registrar = async (req, res) => {
     
-    const usuario = await Usuario.create(req.body)
+    // Validción del Formulario de Registro
+    await check('nombre').notEmpty().withMessage('El nombre es Obligario').run(req)
+    await check('email').isEmail().withMessage('El Email no es valido').run(req)
+    await check('password').isLength({ min: 6}).withMessage('El password tiene que ser al menos de 6 caracteres').run(req)
+    await check('repetir-password').equals('password').withMessage('Los password no son iguales').run(req)
 
+    let resultado = validationResult(req)
+
+    //Verificar que el resultado este Vacio
+    if(!resultado.isEmpty()){
+
+        return res.render('auth/registro',{
+            pagina: 'Crear Cuenta',
+            errores: resultado.array()
+        })
+    }
+
+    const usuario = await Usuario.create(req.body)
     res.json(usuario)
 
 }
