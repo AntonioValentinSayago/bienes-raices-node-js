08634054a46fd50201025ef50f5d44a7300e1812
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-14 11:45:27 -0600

    Validacion del Correo si existe en la Base de datos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -18,10 +18,10 @@ const formularioResgistro = (req, res) => {
 const registrar = async (req, res) => {
     
     // Validción del Formulario de Registro
-    await check('nombre').notEmpty().withMessage('El nombre es Obligario').run(req)
-    await check('email').isEmail().withMessage('El Email no es valido').run(req)
-    await check('password').isLength({ min: 6}).withMessage('El password tiene que ser al menos de 6 caracteres').run(req)
-    await check('repetir-password').equals('password').withMessage('Los password no son iguales').run(req)
+    await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+    await check('password').isLength({ min: 6 }).withMessage('El Password debe ser de al menos 6 caracteres').run(req)
+    await check('repetir_password').equals('password').withMessage('Los Passwords no son iguales').run(req)
 
     let resultado = validationResult(req)
 
@@ -30,12 +30,37 @@ const registrar = async (req, res) => {
 
         return res.render('auth/registro',{
             pagina: 'Crear Cuenta',
-            errores: resultado.array()
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
         })
     }
 
-    const usuario = await Usuario.create(req.body)
-    res.json(usuario)
+    // ? Extraer los Datos
+    const { nombre, email,password } = req.body
+
+    // ? Verificar que el usuario no este duplicado
+    const existeUsuario = await Usuario.findOne( { where: { email: email }})
+
+    if (existeUsuario) {
+        return res.render('auth/registro',{
+            pagina: 'Crear Cuenta',
+            errores: [{ msg: 'El usuario ya esta Registrado' }],
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        })
+    }
+    
+    await Usuario.create({
+        nombre,
+        email,
+        password,
+        token: 123
+    })
 
 }
 
