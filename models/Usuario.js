3120754b13b01f3e9a2f5514dc5504a0551ff17c
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-14 11:45:56 -0600

    Escriptacion del Password con BCRYPTIN

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,4 +1,5 @@
 import { DataTypes, Sequelize } from 'sequelize'
+import bcrypt from 'bcrypt'
 import db from '../config/db.js'
 
 const Usuario = db.define('usuarios', {
@@ -18,6 +19,15 @@ const Usuario = db.define('usuarios', {
     token: DataTypes.STRING,
     confirmado: DataTypes.BOOLEAN
 
-})
+},{
+    hooks: {
+        beforeCreate: async function(usuario) {
+            const salt = await bcrypt.genSalt(10)
+            usuario.password = await bcrypt.hash( usuario.password, salt )
+        }
+    }
+}
+
+)
 
 export default Usuario;
