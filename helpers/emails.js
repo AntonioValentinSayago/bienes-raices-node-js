734eaacc2d6e0d6cf21bf84646646af4c6c4cf26
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-15 13:23:50 -0600

    FILE to la validacion y configuracion del EMAIL

---
+++ helpers/emails.js
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+const emailRegistro = async (datos) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const { email, nombre, token } = datos
+    
+    //? Enviar EMAIL
+    await transport.sendMail( {
+        from:' Bienes Raices',
+        to: email,
+        subject: 'Confirma tu Cuenta en Bienes Raices',
+        text: 'Confirma tu cuenta en Bienes Raices',
+        html: `
+            <p>Hola ${nombre}, comprueba tu cuenta en Bienes Raices <p>
+            <a href="">Confirma tu cuenta en Bienes Raices</a>
+            <p>Si tu no creaste esta cuentam, puedes ignorar este Mensaje
+        `
+    })
+
+}
+
+export {
+    emailRegistro,
+}
