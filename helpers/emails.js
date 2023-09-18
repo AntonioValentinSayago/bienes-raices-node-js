6a523915d9cf5aa3c35e4bd7e32adc3ffa6f4051
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-18 17:08:13 -0600

    Funcion para el reset del password y enviar por correo

--- helpers/emails.js
+++ helpers/emails.js
@@ -27,6 +27,35 @@ const emailRegistro = async (datos) => {
 
 }
 
+const emailOlvidePassword = async (datos) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const { email, nombre, token } = datos
+    
+    //? Enviar EMAIL
+    await transport.sendMail( {
+        from:' Bienes Raices',
+        to: email,
+        subject: 'Restablce tu Password en Bienes Raices',
+        text: 'Restablce tu Password en Bienes Raices',
+        html: `
+            <p>Hola ${nombre}, has solicitado restabalcer tu password en Bienes Raices <p>
+            <p>Sigue el siguiente enlace para genera el password nuevo </p>
+            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/olvide-password/${token}">Restablecer Password en Bienes Raices</a>
+            <p>Si no reconoces este movimiento, ignorar este mensaje</p>
+        `
+    })
+
+}
+
 export {
     emailRegistro,
+    emailOlvidePassword
 }
