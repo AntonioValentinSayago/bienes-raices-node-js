d7f6b9f2ebc96023ed1fd2ecc4593b44dad8cb15
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-19 10:34:02 -0600

    Funcion para generar el TOKEN

--- helpers/tokes.js
+++ helpers/tokes.js
@@ -1,6 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+const generarJWT = id => jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '1d'})
 
 const generarId  = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
 
 export {
+    generarJWT,
     generarId
 }
