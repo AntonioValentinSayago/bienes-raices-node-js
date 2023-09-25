eefd573c4244515097720ecace0195c218a94d79
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-25 17:10:24 -0600

    Implementacion de las validaciones directamente las rutas

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,9 +1,23 @@
 import express from 'express';
-import {admin, crear} from '../controllers/propíedadController.js';
+import { body } from 'express-validator';
+import {admin, crear, guardar} from '../controllers/propíedadController.js';
 
 const router = express.Router();
 
 router.get('/mis-propiedades', admin)
 router.get('/propiedades/crear', crear)
+router.post('/propiedades/crear', 
+    body('titulo').notEmpty().withMessage('El Titulo del Anuncio es Obligatorio'),
+    body('descripcion')
+        .notEmpty().withMessage('La Descripción no puede ir vacia')
+        .isLength({ max: 200 }).withMessage('La Descripción es muy larga'),
+    body('categoria').isNumeric().withMessage('Selecciona una categoría'),
+    body('precio').isNumeric().withMessage('Selecciona un rango de Precios'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la Cantidad de Habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la Cantidad de Estacionamientos'),
+    body('wc').isNumeric().withMessage('Selecciona la Cantidad de Baños'),
+    body('lat').notEmpty().withMessage('Ubica la Propiedad en el Mapa'),
+    guardar
+)
 
 export default router
