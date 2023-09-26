6bc3a17a896d4469400c9b5e96dd1905eb1dc437
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-26 17:47:35 -0600

    Configuracion del Path del Mapa

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,6 @@ export default {
     mode: 'development', 
     entry: {
         mapa: './src/js/mapa.js',
-        agregarImagen: './src/js/agregarImagen.js',
-        mostrarMapa: './src/js/mostrarMapa.js',
-        mapaInicio: './src/js/mapaInicio.js',
-        cambiarEstado: './src/js/cambiarEstado.js',
     },
     output: {
         filename: '[name].js',
