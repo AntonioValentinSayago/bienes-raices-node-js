86dea6568fefff5462aff18dda2535331574b636
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-27 12:04:26 -0600

    Nuevo Scope, para mostrar datos especificos

--- models/Usuario.js
+++ models/Usuario.js
@@ -25,6 +25,13 @@ const Usuario = db.define('usuarios', {
             const salt = await bcrypt.genSalt(10)
             usuario.password = await bcrypt.hash( usuario.password, salt )
         }
+    },
+    scopes: {
+        eliminarPassword: {
+            attributes: {
+                exclude: ['password', 'token', 'confirmado', 'createAt', 'updateAt']
+            }
+        }
     }
 })
 
