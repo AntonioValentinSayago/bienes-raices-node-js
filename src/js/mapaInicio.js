294942010032ab764299144415389cf1d41d872c
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-04 20:28:56 -0600

    Archivos de configuracion para la implementacion del mapa

---
+++ src/js/mapaInicio.js
@@ -0,0 +1,9 @@
+(function() {
+    const lat = 34.040967;
+    const lng = -118.1618621;
+    const mapa = L.map('mapa-inicio').setView([lat, lng ], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa)
+})
