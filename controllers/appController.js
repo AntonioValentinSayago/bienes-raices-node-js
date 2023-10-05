f7736c4dd54a3a9e52621d931ac64abdfec6b843
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-04 20:29:43 -0600

    Nueva ruta y controlador para la configuracion de la vista principal

---
+++ controllers/appController.js
@@ -0,0 +1,24 @@
+const inicio = ( req, res ) => {
+    res.render('inicio', {
+        pagina: 'Inicio'
+    })
+}
+
+const categorias = ( req, res ) => {
+    
+}
+
+const noEncontrado = ( req, res ) => {
+    
+}
+
+const buscador = ( req, res ) => {
+    
+}
+
+export{
+    inicio,
+    categorias,
+    noEncontrado,
+    buscador
+}
