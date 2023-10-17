c70af992dfd798849dbfb7037bec1d756fff4db4
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-17 17:52:29 -0600

    Terminando la configuracion del Filtro de Casas y departamentos

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,15 +2,41 @@ import { Precio,Categoria, Propiedad } from '../models/index.js'
 
 const inicio = async ( req, res ) => {
 
-    const [ categorias, precios ] = await Promise.all([
+    const [ categorias, precios, casas, departamentos ] = await Promise.all([
         Categoria.findAll({ raw: true}),
-        Precio.findAll({ raw: true})
+        Precio.findAll({ raw: true}),
+        Propiedad.findAll({
+            limit:3,
+            where:{
+                categoriaId:1
+            },
+            include: [
+                {
+                    model: Precio, as: 'precio'
+                }
+            ],
+            order: [['createdAT', 'DESC']]
+        }),
+        Propiedad.findAll({
+            limit:3,
+            where:{
+                categoriaId: 2
+            },
+            include: [
+                {
+                    model: Precio, as: 'precio'
+                }
+            ],
+            order: [['createdAt', 'DESC']]
+        }),
     ])
 
     res.render('inicio', {
         pagina: 'Inicio', 
         categorias,
-        precios
+        precios,
+        casas,
+        departamentos
     })
 }
 
