86023fe982795a0d5db7c1e7a33803c56e240928
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-16 20:56:17 -0600

    Ultimos Cambios para la configuracion de los Pines en el mapa

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -1,7 +1,19 @@
-(function() {
+(function () {
     const lat = 34.040967;
     const lng = -118.1618621;
-    const mapa = L.map('mapa-inicio').setView([lat, lng ], 13);
+    const mapa = L.map('mapa-inicio').setView([lat, lng], 13);
+
+    let markers = new L.FeatureGroup().addTo(mapa)  //* Crea la capa arriba del mapa para mostrar el pin en la posición
+
+    let propiedades = [];
+    // * Filtros para selecionar los precios 
+    const filtros = {
+        categoria: '',
+        precio: ''
+    }
+
+    const categoriasSelect = document.querySelector('#categorias');
+    const preciosSelect = document.querySelector('#precios');
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -17,6 +29,49 @@
             console.log(error)
         }
     }
+
+    // * Filtrado de Categorias y precios
+    categoriasSelect.addEventListener('change', e => {
+        filtros.categoria = +e.target.value
+        filtrarPropiedades();
+    })
+
+    preciosSelect.addEventListener('change', e => {
+        filtros.precio = +e.target.value
+        filtrarPropiedades();
+    })
+
+    // * Mostrar las propíedades en la vista
+    const mostrarPropiedades = propiedades => {
+
+        // Limpiar los markers previos
+        markers.clearLayers()
+
+        propiedades.forEach(propiedad => {
+            // * Agregar los Pines
+            const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
+                autoPan: true
+            })
+                .addTo(mapa)
+                .bindPopup(`
+                <p class="text-indigo-600 font-bold">${propiedad.categoria.nombre}</p>
+                <h1 class="text-xl font-extrabold uppercase my-2">${propiedad?.titulo}</h1>
+                <img src="/uploads/${propiedad?.imagen}" alt="Imagen de la propiedad ${propiedad.titulo}">
+                <p class="text-gray-600 font-bold">${propiedad.precio.nombre}</p>
+                <a href="/propiedad/${propiedad.id}" class="bg-indigo-600 text-slate-100 block p-2 text-center font-bold uppercase">Ver Propiedad</a>
+            `)
+            markers.addLayer(marker)
+        })
+    }
+
+    const filtrarPropiedades = () => {
+        const resultado = propiedades.filter(filtrarCategoria).filter(filtrarPrecio)
+        mostrarPropiedades(resultado)
+    }
+
+    const filtrarCategoria = propiedad => filtros.categoria ? propiedad.categoriaId === filtros.categoria : propiedad
+
+    const filtrarPrecio = propiedad => filtros.precio ? propiedad.precioId === filtros.precio : propiedad
 
     obtenerPropiedades()
 
