231c6eb56057b3b3d44599ab9e4f1b9703fb374d
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-17 18:08:14 -0600

    Se configura la ruta de la pagina 'Error 404' para las categorias

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,14 +1,14 @@
-import { Precio,Categoria, Propiedad } from '../models/index.js'
+import { Precio, Categoria, Propiedad } from '../models/index.js'
 
-const inicio = async ( req, res ) => {
+const inicio = async (req, res) => {
 
-    const [ categorias, precios, casas, departamentos ] = await Promise.all([
-        Categoria.findAll({ raw: true}),
-        Precio.findAll({ raw: true}),
+    const [categorias, precios, casas, departamentos] = await Promise.all([
+        Categoria.findAll({ raw: true }),
+        Precio.findAll({ raw: true }),
         Propiedad.findAll({
-            limit:3,
-            where:{
-                categoriaId:1
+            limit: 3,
+            where: {
+                categoriaId: 1
             },
             include: [
                 {
@@ -18,8 +18,8 @@ const inicio = async ( req, res ) => {
             order: [['createdAT', 'DESC']]
         }),
         Propiedad.findAll({
-            limit:3,
-            where:{
+            limit: 3,
+            where: {
                 categoriaId: 2
             },
             include: [
@@ -32,7 +32,7 @@ const inicio = async ( req, res ) => {
     ])
 
     res.render('inicio', {
-        pagina: 'Inicio', 
+        pagina: 'Inicio',
         categorias,
         precios,
         casas,
@@ -40,19 +40,44 @@ const inicio = async ( req, res ) => {
     })
 }
 
-const categorias = ( req, res ) => {
-    
+const categorias = async (req, res) => {
+    const { id } = req.params
+
+    //* Comprobar que la categoría exista
+    const categoria = await Categoria.findByPk(id)
+    if (!categoria) {
+        return res.redirect('/404')
+    }
+
+    //* Obtener las catrgorias existentes
+    const propiedades = await Propiedad.findAll({
+        where: {
+            categoriaId: id
+        },
+        include: [
+            { model: Precio, as: 'precio' }
+        ]
+    })
+
+    res.render('categoria', {
+        pagina: `${categoria.nombre}s en Venta`,
+        propiedades
+    })
+
 }
 
-const noEncontrado = ( req, res ) => {
-    
+const noEncontrado = (req, res) => {
+    res.render('404', {
+        pagina: 'No Encontrada',
+        csrfToken: req.csrfToken()
+    })
 }
 
-const buscador = ( req, res ) => {
-    
+const buscador = (req, res) => {
+
 }
 
-export{
+export {
     inicio,
     categorias,
     noEncontrado,
