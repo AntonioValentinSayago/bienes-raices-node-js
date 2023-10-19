ecc7bbce76fa780ac50c66dae9bf749a3e95672d
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-19 17:59:49 -0600

    Formatear la fecha y mostrarla en la vista de los mensajes

--- models/index.js
+++ models/index.js
@@ -2,21 +2,21 @@ import Propiedad from './Propiedad.js'
 import Precio from './Precio.js'
 import Categoria from './Categoria.js'
 import Usuario from './Usuario.js'
-//import Mensaje from './Mensaje.js'
+import Mensaje from './Mensaje.js'
 
 Propiedad.belongsTo(Precio, { foreignKey: 'precioId'})
 Propiedad.belongsTo(Categoria, { foreignKey: 'categoriaId'})
 Propiedad.belongsTo(Usuario, { foreignKey: 'usuarioId'})
-//Propiedad.hasMany(Mensaje, { foreignKey: 'propiedadId'} )
+Propiedad.hasMany(Mensaje, { foreignKey: 'propiedadId'} )
 
-// Mensaje.belongsTo(Propiedad, { foreignKey: 'propiedadId'})
-// Mensaje.belongsTo(Usuario, { foreignKey: 'usuarioId'})
+Mensaje.belongsTo(Propiedad, { foreignKey: 'propiedadId'})
+Mensaje.belongsTo(Usuario, { foreignKey: 'usuarioId'})
 
 
 export {
     Propiedad,
     Precio,
     Categoria,
-    Usuario
-    //Mensaje
+    Usuario,
+    Mensaje
 }
